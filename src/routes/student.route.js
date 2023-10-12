63fc39dde3236b8ee562ece759350f9095960368
Teza Alfian <44139001+tezaalfian@users.noreply.github.com>
2023-10-12 07:16:05 +0700

    feat: add retrieve & update student

---
+++ src/routes/student.route.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import studentController from '../controllers/student.controller.js';
+import authMiddleware from '../middlewares/authMiddleware.js';
+import authorize from '../middlewares/authorizeMiddleware.js';
+
+const studentRouter = express.Router();
+
+studentRouter.use(authMiddleware);
+studentRouter.get('/', authorize('guru'), studentController.getAllStudent);
+studentRouter.get('/:nomorInduk([0-9]{10})', authorize('guru'), studentController.getStudentDetail);
+studentRouter
+    .route('/profile')
+    .all(authorize('siswa'))
+    .get(studentController.getStudentProfile)
+    .patch(studentController.updateStudentProfile);
+
+export default studentRouter;
